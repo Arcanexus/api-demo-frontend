0d121678438a3f19b657093eb866817c217a9fad
Alexandre Schwartzmann <schwartzmann.a@gmail.com>
2023-02-15 15:51:23 +0100

    more robust and more sexy

--- app.py
+++ app.py
@@ -1,43 +1,52 @@
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-import requests, json
+from flask import Flask, render_template, request, send_from_directory
+import requests, json, os
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+  HTTPMethod = ''
+  targetURL = ''
+  payload = ''
+  result = ''
+
   if request.method == 'POST':
     HTTPMethod = request.form.get('HTTPMethod')
     targetURL = request.form.get('targetURL')
-    payload = json.loads(request.form.get('payload'))
+    try:
+      # payload = json.loads(request.form.get('payload'))
+      payload = request.form.get('payload')
+    except:
+      payload = '{}'
+    
     
     match HTTPMethod:
       case "GET":
         result = requests.get(targetURL)
       case "POST":
-        result = requests.post(targetURL, data=json.dumps(payload))
+        result = requests.post(targetURL, data=payload) # , data=json.dumps(payload))
       case "PUT":
-        result = requests.put(targetURL, data=json.dumps(payload))
+        result = requests.put(targetURL, data=payload)
       case "PATCH":
-        result = requests.patch(targetURL, data=json.dumps(payload))
+        result = requests.patch(targetURL, data=payload)
       case "DELETE":
-        result = requests.delete(targetURL, data=json.dumps(payload))
+        result = requests.delete(targetURL, data=payload)
       case _:
         return "ERROR, HTTP Method not in (GET, POST, PUT, PATCH, DELETE)", 400
-    
-    return '''
-              <h1><u>Request:</u></h1>
-              <h2>HTTP Method: {}</h2>
-              <h2>Target URL: {}</h2>
-              <h2>Payload: {}</h2>
-              <hr \>
-              <a href="javascript:history.back()">Go Back</a><br \>
-              <h1><u>Result:</u></h1><h2> {} {}<p>Content:</h2></p>
-              {}'''.format(HTTPMethod, targetURL, payload, result.status_code, result.reason, result.text)
-   
-  print('Request for index page received')
-  return render_template('index.html')
+
+    # return '''
+    #           <h1><u>Request:</u></h1>
+    #           <h2>HTTP Method: {}</h2>
+    #           <h2>Target URL: {}</h2>
+    #           <h2>Payload: {}</h2>
+    #           <hr \>
+    #           <a href="javascript:history.back()">Go Back</a><br \>
+    #           <h1><u>Result:</u></h1><h2> {} {}<p>Content:</h2></p>
+    #           {}'''.format(HTTPMethod, targetURL, payload, result.status_code, result.reason, result.text)
+  # print('Request for index page received')
+  return render_template('index.html', HTTPMethod=HTTPMethod, targetURL=targetURL, payload=payload, result=result)
 
 @app.route('/favicon.ico')
 def favicon():
