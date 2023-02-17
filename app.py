a7777380420a2ad6fa6cb940aabf0f7fe06cca56
Alexandre Schwartzmann <schwartzmann.a@gmail.com>
2023-02-17 12:54:49 +0100

    code cleaning

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from datetime import datetime
 from flask import Flask, render_template, request, send_from_directory
-import requests, json, os
+import requests, os
 
 app = Flask(__name__)
 
@@ -16,17 +15,15 @@ def index():
     HTTPMethod = request.form.get('HTTPMethod')
     targetURL = request.form.get('targetURL')
     try:
-      # payload = json.loads(request.form.get('payload'))
       payload = request.form.get('payload')
     except:
       payload = '{}'
-    
     
     match HTTPMethod:
       case "GET":
         result = requests.get(targetURL)
       case "POST":
-        result = requests.post(targetURL, data=payload) # , data=json.dumps(payload))
+        result = requests.post(targetURL, data=payload)
       case "PUT":
         result = requests.put(targetURL, data=payload)
       case "PATCH":
@@ -36,23 +33,12 @@ def index():
       case _:
         return "ERROR, HTTP Method not in (GET, POST, PUT, PATCH, DELETE)", 400
 
-    # return '''
-    #           <h1><u>Request:</u></h1>
-    #           <h2>HTTP Method: {}</h2>
-    #           <h2>Target URL: {}</h2>
-    #           <h2>Payload: {}</h2>
-    #           <hr \>
-    #           <a href="javascript:history.back()">Go Back</a><br \>
-    #           <h1><u>Result:</u></h1><h2> {} {}<p>Content:</h2></p>
-    #           {}'''.format(HTTPMethod, targetURL, payload, result.status_code, result.reason, result.text)
-  # print('Request for index page received')
   return render_template('index.html', HTTPMethod=HTTPMethod, targetURL=targetURL, payload=payload, result=result)
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80)
